540a8bb6a1c7d77f9608fd939f6a8b7c9da15435
Steven Barrios <stevenbarrios03@gmail.com>
2024-05-13 22:33:29 -0400

    Refactor SpotifyProvider import logic
    authorization endpoint needs to be set prior to providers declaration

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -10,12 +10,27 @@ if (!process.env.SPOTIFY_CLIENT_SECRET) {
   throw new Error("Missing SPOTIFY_CLIENT_SECRET is missing");
 }
 
+// Need to init Spotify Provider first, then import it inside providers array
+const spotifyProvider = SpotifyProvider({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+});
+const authURL = new URL("https://accounts.spotify.com/authorize");
+
+const scopes = [
+  "user-read-email",
+  "user-read-private",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "playlist-read-private",
+];
+
+authURL.searchParams.append("scope", scopes.join(" "));
+spotifyProvider.authorization = authURL.toString();
+
 export const authOptions: NextAuthOptions = {
   providers: [
-    SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET
-    })
+    spotifyProvider
   ],
   debug: process.env.NODE_ENV === "development",
   secret: process.env.NEXTAUTH_SECRET,
