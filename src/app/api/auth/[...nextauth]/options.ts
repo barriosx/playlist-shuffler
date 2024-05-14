50a65e0780f9cff6dee7b7b4c39b8baba8f96899
Steven Barrios <stevenbarrios03@gmail.com>
2024-05-13 22:21:00 -0400

    Bring in spotify OAuth Client Provider

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,20 @@
 import type { NextAuthOptions } from "next-auth";
+import SpotifyProvider from 'next-auth/providers/spotify'; 
+// Using OAuth Client Credentials flow 
+
+if (!process.env.SPOTIFY_CLIENT_ID) {
+  throw new Error("SPOTIFY_CLIENT_ID is missing");
+}
+
+if (!process.env.SPOTIFY_CLIENT_SECRET) {
+  throw new Error("Missing SPOTIFY_CLIENT_SECRET is missing");
+}
 
 export const authOptions: NextAuthOptions = {
-    providers: []
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+    })
+  ]
 }
