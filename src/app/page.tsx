8ceb34d6894372b37bb3514d2528a502879bb3fe
Steven Barrios <stevenbarrios03@gmail.com>
2024-05-15 19:38:37 -0400

    Make container dynamic height of viewport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import Playlists from "@/components/playlists";
 export default function Home() {
 
   return (
-    <>
+    <div className="h-dvh flex-col">
       <Header></Header>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex items-center justify-between px-8">
         <Playlists></Playlists>
         <p>Play your music and keep your playlists organized.</p>
       </main>
-    </>
+    </div>
   );
 }
