02e196ce5097d34363c1935b6ecccabef30dc7d6
Steven Barrios <stevenbarrios03@gmail.com>
2024-05-17 07:35:46 -0400

    Add reference for player component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/header";
+import Player from "@/components/player";
 import Playlists from "@/components/playlists";
 
 export default function Home() {
@@ -8,7 +9,8 @@ export default function Home() {
       <Header></Header>
       <main className="flex items-center justify-between px-8">
         <Playlists></Playlists>
-        <p>Play your music and keep your playlists organized.</p>
+        <p>Shuffle Play music across your playlist library</p> 
+        <Player/>
       </main>
     </div>
   );
